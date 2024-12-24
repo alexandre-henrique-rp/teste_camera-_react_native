bff71565c7024f31cf14f8eb83e1ac03f2b6fd2d
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-12-24 16:54:08 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,146 +1,37 @@
-import {
-  CameraType,
-  CameraView,
-  useCameraPermissions,
-  useMicrophonePermissions
-} from "expo-camera";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useRef, useState } from "react";
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import * as Sharing from 'expo-sharing';
-import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect, useState } from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Accelerometer } from "expo-sensors";
 
 export default function App() {
-  const [facing, setFacing] = useState<CameraType>("front");
-  const [permission, requestPermission] = useCameraPermissions();
-  const [micPermission, requestMicPermission] = useMicrophonePermissions();
-  const [recording, setRecording] = useState(false);
-  const [videoUri, setVideoUri] = useState<string | null>(null);
-  const [time, setTime] = useState(0);
-  const CanRef = useRef<null | any>(null);
-
-  async function changeScreenOrientation() {
-    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-  }
-
-  async function deviceOrientation() {
-    const status = await ScreenOrientation.getPlatformOrientationLockAsync();
-    console.log(status);
-  }
+  const [{ x }, setAcceleration] = useState({ x: 0 });
+  const [Status, setStatus] = useState(0);
 
   useEffect(() => {
-    requestPermission();
-    requestMicPermission();
-    if (recording) {
-      const interval = setInterval(() => {
-        setTime((time) => time + 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-    // changeScreenOrientation();
-    deviceOrientation();
-
-  }, [recording, requestPermission, requestMicPermission]);
-
-  if (!permission || !micPermission) {
-    return null;
-  }
-
-  function toggleCameraFacing() {
-    setFacing((current) => (current === "back" ? "front" : "back"));
-  }
-  async function startCapture() {
-    if (!CanRef.current) return;
-
-    setRecording(true);
-    try {
-      const video = await CanRef.current.recordAsync({
-        maxDuration: 15
-      });
-      setRecording(false);
-      setTime(0);
-      console.log("Video captured", video);
-      // salvar video em algum lugar
-      setVideoUri(video.uri);
-    } catch (error) {
-      console.error("Error recording video:", error);
-    }
-  }
-
-  async function stopCapture() {
-    setRecording(false);
-    if (CanRef.current) {
-      try {
-        await CanRef.current.stopRecording();
-      } catch (error) {
-        console.error("Error stopping recording:", error);
+    const subscription = Accelerometer.addListener(setAcceleration);
+    // Accelerometer.setUpdateInterval(2000);
+    (async () => {
+      if (x > 0.8 && Status != 1) {
+        setStatus(1);
+        await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+      } else if (x < -0.8 && Status != 2) {
+        setStatus(2);
+        await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+      } else if (x < 0.8 && x > -0.8 && Status != 0) {
+        setStatus(0); 
+        await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
       }
-    }
-  }
+    })();
+    return () => subscription.remove();
+  }, [x, Status]);
 
-  async function shareVideo() {
-    if (!videoUri) {
-      Alert.alert("Erro", "Nenhum vídeo disponível para compartilhar");
-      return;
-    }
+ 
 
-    try {
-      const isAvailable = await Sharing.isAvailableAsync();
-      
-      if (!isAvailable) {
-        Alert.alert("Erro", "Compartilhamento não está disponível neste dispositivo");
-        return;
-      }
-
-      await Sharing.shareAsync(videoUri, {
-        mimeType: 'video/mp4',
-        dialogTitle: 'Compartilhar vídeo',
-        UTI: 'public.movie'  // necessário para iOS
-      });
-    } catch (error) {
-      console.error("Error sharing video:", error);
-      Alert.alert("Erro", "Não foi possível compartilhar o vídeo");
-    }
-  }
 
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <CameraView
-        style={styles.camera}
-        facing={facing}
-        mode={"video"}
-        ref={CanRef}
-        ratio="16:9"
-        videoQuality="1080p"
-      >
-        <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-            style={styles.button2}
-            onPress={shareVideo}
-            disabled={!videoUri}
-          >
-            <FontAwesome 
-              name="share" 
-              size={30} 
-              color={videoUri ? "white" : "gray"} 
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonCapture}
-            onPress={recording ? stopCapture : startCapture}
-            disabled={recording}
-          >
-            {recording ? (
-              <Text style={{ color: "white", fontSize: 20 }}>{time}</Text>
-            ) : null}
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <FontAwesome name="refresh" size={30} color="white" />
-          </TouchableOpacity>
-        </View>
-      </CameraView>
+      <Text>teste posição da tela</Text>
     </View>
   );
 }
@@ -148,7 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+    alignItems: "center"
   },
   camera: {
     flex: 1
